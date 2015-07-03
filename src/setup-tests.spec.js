f3ad247a872584e51cbe4d1d8a1334c7e169a830
Peter Marton <peter@risingstack.com>
2015-07-03 16:15:21 +0200

    Remove unused chai plugins

--- src/setup-tests.spec.js
+++ src/setup-tests.spec.js
@@ -3,12 +3,8 @@
 var sinon = require('sinon');
 var chai = require('chai');
 var sinonChai = require('sinon-chai');
-var chaiSubset = require('chai-subset');
-var chaiAsPromised = require('chai-as-promised');
 
 before(function () {
-  chai.use(chaiAsPromised);
-  chai.use(chaiSubset);
   chai.use(sinonChai);
 
   sinon.stub.returnsWithResolve = function (data) {
