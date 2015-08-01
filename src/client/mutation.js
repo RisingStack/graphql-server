e11d05e947ff079f73bcdaa8bded98104acd4a41
Randy Lubin <randylubin@gmail.com>
2015-07-31 18:25:37 -0700

    added create and delete

--- src/client/mutation.js
+++ src/client/mutation.js
@@ -28,3 +28,47 @@ request
     debug(err || res.body);
     debug('friends', res.body.data.updateUser.friends);
   });
+
+request
+  .post('http://localhost:3000/data')
+  .send({
+    query: `
+    mutation M($name: String!) {
+      createUser(name: $name) {
+        name
+        friends {
+          name
+        }
+      }
+    }
+    `,
+    params: {
+      name: 'Randy'
+    }
+  })
+  .end(function (err, res) {
+    debug(err || res.body);
+    debug('friends', res.body);
+  });
+
+request
+  .post('http://localhost:3000/data')
+  .send({
+    query: `
+    mutation M($userId: String!) {
+      deleteUser(id: $userId) {
+        name
+        friends {
+          name
+        }
+      }
+    }
+    `,
+    params: {
+      userId: '55bc19a2658fa5be4990f532'
+    }
+  })
+  .end(function (err, res) {
+    debug(err || res.body);
+    debug('friends', res.body);
+  });
